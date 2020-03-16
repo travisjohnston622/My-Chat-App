0eb9ba18b96e45e0c97c853f00f88e9a3362bc5a
Travis Johnston <travisjohnston4588@icloud.com>
2020-03-15 20:48:01 -0500

    update to app.js back-ground color

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 	render() {
 		return (
 			<Router>
-				<div>
+				<div className='container'>
 					<Nav />
 					<Switch>
 						{/* Visiting localhost:3000 will redirect to localhost:3000/home */}
